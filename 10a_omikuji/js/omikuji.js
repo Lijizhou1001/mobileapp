0b5e8a344651c4ffef449924bf6939d7652afbee
Lijizhou1001 <linyuanzhou7@gmail.com>
2025-07-15 14:20:33 +0900

    Add files via upload

---
+++ 10a_omikuji/js/omikuji.js
@@ -0,0 +1,103 @@
+"use strict";
+
+// サウンドフラグの初期化
+let soundEndflag = "0";
+let w_sound;
+let music;
+
+window.addEventListener("DOMContentLoaded", function () {
+    // ヘッダーアニメーション（textillate + animate.css）
+    $("header").textillate({
+        loop: false,
+        minDisplayTime: 2000,
+        initialDelay: 2000,
+        autoStart: true,
+        in: {
+            effect: "fadeInLeftBig",
+            delayScale: 1.5,
+            delay: 50,
+            sync: false,
+            shuffle: true
+        }
+    });
+
+    // おみくじボタンのフェードイン表示（ScrollReveal）
+    ScrollReveal().reveal("#btn1", { duration: 9000 });
+
+    // 5秒後にポップアップメッセージ
+    setTimeout(function () {
+        let popMessage = "いらっしゃい！おみくじ引いてって！";
+        window.alert(popMessage);
+    }, 5000);
+}, false);
+
+// おみくじボタンの処理
+const btn1 = document.getElementById("btn1");
+const omikujiText = document.getElementById("omikujiText");
+
+btn1.addEventListener("click", function () {
+    // 既に音が再生されているなら止める
+    if (soundEndflag === "1") {
+        soundControl("end", "");
+    }
+
+    // おみくじの結果データ
+    const resultText = ["大吉!!!!!", "吉!!!!", "中吉!!!", "小吉!!", "末吉!", "凶。。"];
+    const resultColor = ["#ff0000", "#c71585", "#ff1493", "#ff69b4", "#ff8c00", "#1e90ff"];
+    const resultFontSize = ["90px", "80px", "70px", "60px", "50px", "40px"];
+    const resultMaxSpeed = [10, 10, 8, 5, 5, 5];
+    const resultMaxSize = [30, 30, 20, 15, 20, 20];
+    const resultImage = [
+        "img/star.png",
+        "img/sakura_hanabira.png",
+        "img/sakura_hanabira.png",
+        "img/sakura_hanabira.png",
+        "img/leaf.png",
+        "img/snowflakes.png"
+    ];
+    const resultSound = [
+        "sound/omikuji_sound1.mp3",
+        "sound/omikuji_sound2.mp3",
+        "sound/omikuji_sound3.mp3",
+        "sound/omikuji_sound4.mp3",
+        "sound/omikuji_sound5.mp3",
+        "sound/omikuji_sound2.mp3"
+    ];
+
+    // ランダムに結果を決定
+    const n = Math.floor(Math.random() * resultText.length);
+
+    // 結果表示
+    omikujiText.textContent = resultText[n];
+    omikujiText.style.color = resultColor[n];
+    omikujiText.style.fontSize = resultFontSize[n];
+
+    // 音声再生
+    w_sound = resultSound[n];
+    soundControl("start", w_sound);
+    soundEndflag = "1";
+
+    // エフェクト初期化と新たに開始
+    $(document).snowfall("clear");
+    $(document).snowfall({
+        maxSpeed: resultMaxSpeed[n],
+        minSpeed: 1,
+        maxSize: resultMaxSize[n],
+        minSize: 1,
+        image: resultImage[n]
+    });
+}, false);
+
+// 音声コントロール関数
+function soundControl(status, w_sound) {
+    if (status === "start") {
+        music = new Audio(w_sound);
+        music.currentTime = 0;
+        music.play();
+    } else if (status === "end") {
+        if (music) {
+            music.pause();
+            music.currentTime = 0;
+        }
+    }
+}
---
+++ 10c_omikuji/js/omikuji.js
@@ -0,0 +1,113 @@
+"use strict";
+let soundEndflag = "0";
+let w_sound;
+let music;
+let n = "";
+let nBefore = "";
+window.addEventListener("DOMContentLoaded", function () {
+    // ヘッダーアニメーション（textillate + animate.css）
+    $("header").textillate({
+        loop: false,
+        minDisplayTime: 2000,
+        initialDelay: 2000,
+        autoStart: true,
+        in: {
+            effect: "fadeInLeftBig",
+            delayScale: 1.5,
+            delay: 50,
+            sync: false,
+            shuffle: true
+        }
+    });
+
+    // おみくじボタンのフェードイン表示（ScrollReveal）
+    ScrollReveal().reveal("#btn1", { duration: 9000 });
+
+    // 5秒後にポップアップメッセージ
+    setTimeout(function () {
+        let popMessage = "いらっしゃい！おみくじ引いてって！";
+        window.alert(popMessage);
+    }, 5000);
+}, false);
+
+// おみくじボタンの処理
+const btn1 = document.getElementById("btn1");
+const omikujiText = document.getElementById("omikujiText");
+const omikujiTextImage = document.getElementById("omikujiTextImage");
+
+btn1.addEventListener("click", function () {
+    // 既に音が再生されているなら止める
+    if (soundEndflag === "1") {
+        soundControl("end", "");
+    }
+
+    // おみくじの結果データ
+    const resultText = [
+        "img/daikichi.png",
+        "img/chukichi.png",
+        "img/syokichi.png",
+        "img/suekichi.png",
+        "img/daikyo.png"
+    ];
+    const resultMaxSpeed = [10, 10, 8, 5, 5];
+    const resultMaxSize = [30, 30, 30, 40, 30];
+    const resultImage = [
+        "img/star.png",
+        "img/sakura_hanabira.png",
+        "img/water1.png",
+        "img/redLeaves4.png",
+        "img/snowflakes.png"
+    ];
+    const resultSound = [
+        "sound/omikuji_sound1.mp3",
+        "sound/omikuji_sound2.mp3",
+        "sound/omikuji_sound3.mp3",
+        "sound/omikuji_sound4.mp3",
+        "sound/omikuji_sound5.mp3"
+    ];
+
+    // ランダムに結果を決定
+    //const n = Math.floor(Math.random() * resultText.length);
+    while (n === nBefore) {
+        n = Math.floor(Math.random() * resultText.length)
+    }
+    nBefore = n;
+    // 結果表示
+    omikujiTextImage.src = resultText[n];
+    omikujiTextImage.classList.add("omikujiPaper");
+
+    omikujiTextImage.addEventListener("animationend",
+        function () {
+            omikujiTextImage.classList.remove("omikujiPaper");
+        }, false
+    );
+
+    // 音声再生
+    w_sound = resultSound[n];
+    soundControl("start", w_sound);
+    soundEndflag = "1";
+
+    // エフェクト初期化と新たに開始
+    $(document).snowfall("clear");
+    $(document).snowfall({
+        maxSpeed: resultMaxSpeed[n],
+        minSpeed: 1,
+        maxSize: resultMaxSize[n],
+        minSize: 1,
+        image: resultImage[n]
+    });
+}, false);
+
+// 音声コントロール関数
+function soundControl(status, w_sound) {
+    if (status === "start") {
+        music = new Audio(w_sound);
+        music.currentTime = 0;
+        music.play();
+    } else if (status === "end") {
+        if (music) {
+            music.pause();
+            music.currentTime = 0;
+        }
+    }
+}
